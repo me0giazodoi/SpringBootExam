a75ad5a5cc718715e99395a6c051ed54d161d2b0
me0giazodoi <hiepga1243@gmail.com>
2024-04-10 15:56:48 +0700

    a

---
+++ src/main/java/com/example/exam_springboot/ExamSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.exam_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamSpringbootApplication.class, args);
+    }
+
+}
